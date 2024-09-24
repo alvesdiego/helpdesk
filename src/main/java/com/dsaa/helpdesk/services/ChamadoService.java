c18fdba113591bee4054bda1c7a716bfbf4b5d40
alvesdiego <diego.aalves@gmail.com>
2024-09-24 18:36:29 -0300

    FindById para Chamado

---
+++ src/main/java/com/dsaa/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.dsaa.helpdesk.services;
+
+import com.dsaa.helpdesk.domain.Chamado;
+import com.dsaa.helpdesk.repositories.ChamadoRepository;
+import com.dsaa.helpdesk.services.exceptions.ObjectnotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID " + id));
+
+    }
+}
