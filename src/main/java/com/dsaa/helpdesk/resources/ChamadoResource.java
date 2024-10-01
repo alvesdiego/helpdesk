7b85ed3bbab796a985dd3c171a3f8b392587e7d6
alvesdiego <diego.aalves@gmail.com>
2024-10-01 15:47:49 -0300

    Create para Chamado

--- src/main/java/com/dsaa/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/dsaa/helpdesk/resources/ChamadoResource.java
@@ -3,14 +3,14 @@ package com.dsaa.helpdesk.resources;
 import com.dsaa.helpdesk.domain.Chamado;
 import com.dsaa.helpdesk.domain.dtos.ChamadoDTO;
 import com.dsaa.helpdesk.services.ChamadoService;
+import jakarta.validation.Valid;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,7 +35,12 @@ public class ChamadoResource {
     }
 
 
-
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+        Chamado obj = service.create(objDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return  ResponseEntity.created(uri).build();
+    }
 
 
 }
--- src/main/java/com/dsaa/helpdesk/services/ChamadoService.java
+++ src/main/java/com/dsaa/helpdesk/services/ChamadoService.java
@@ -1,8 +1,14 @@
 package com.dsaa.helpdesk.services;
 
 import com.dsaa.helpdesk.domain.Chamado;
+import com.dsaa.helpdesk.domain.Cliente;
+import com.dsaa.helpdesk.domain.Tecnico;
+import com.dsaa.helpdesk.domain.dtos.ChamadoDTO;
+import com.dsaa.helpdesk.domain.enums.Prioridade;
+import com.dsaa.helpdesk.domain.enums.Status;
 import com.dsaa.helpdesk.repositories.ChamadoRepository;
 import com.dsaa.helpdesk.services.exceptions.ObjectnotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +20,10 @@ public class ChamadoService {
 
     @Autowired
     private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
 
     public Chamado findById(Integer id){
         Optional<Chamado> obj = repository.findById(id);
@@ -24,4 +34,26 @@ public class ChamadoService {
     public List<Chamado> findAll() {
         return repository.findAll();
     }
+
+    public Chamado create(@Valid ChamadoDTO objDTO) {
+        return repository.save(newChamado(objDTO));
+    }
+
+    private Chamado newChamado(ChamadoDTO obj){
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null){
+            chamado.setId(obj.getId());
+        }
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
+    }
+
 }
